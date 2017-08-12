97911a989269a59cc290b50d7e285eaf8942d8f8
Jan Oliver Rüdiger <e-mail@jan-oliver-ruediger.de>
2017-08-12 16:55:38 +0200

    // commit

---
+++ d14tive.TweetSelectorByUrl/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Bcs.IO;
+
+namespace d14tive.TweetSelectorByUrl
+{
+  class Program
+  {
+    [STAThread]
+    static void Main(string[] args)
+    {
+      var ofd1 = new OpenFileDialog {Filter = "TSV-Datei (*.txt)|*.txt"};
+      ofd1.ShowDialog();
+
+      var ofd2 = new OpenFileDialog {Filter = "TXT-URL-Liste (*.txt)|*.txt"};
+      ofd2.ShowDialog();
+
+      var csv = FileIO.ReadLines(ofd1.FileName);
+      var url = FileIO.ReadLines(ofd2.FileName).Where(x=>!string.IsNullOrWhiteSpace(x));
+
+      var res = new List<string>();
+
+      foreach (var x in url)
+      {
+        foreach (var y in csv)
+        {
+          if (!y.Contains(x))
+            continue;
+          var s = y.Split(new[] {"\t"}, StringSplitOptions.RemoveEmptyEntries);
+          res.Add( s[s.Length - 1]);
+          break;
+        }
+      }
+
+      FileIO.Write(ofd1.FileName + ".txt", res.ToArray());
+    }
+  }
+}
